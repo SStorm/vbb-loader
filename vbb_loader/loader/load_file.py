48f3bad30e73584d69a7c8259006bfb17f74f7ab
Romanas Sonkinas <romanass@gmail.com>
2020-10-22 20:36:53 +0100

    Converted to a class - easier state management

--- vbb_loader/loader/load_file.py
+++ vbb_loader/loader/load_file.py
@@ -12,58 +12,65 @@ LOGGER = setup.logger(__name__)
 BATCH_SIZE = 500
 
 
-def load(file_to_import: str, url: str, user: str, password: str):
-    table = table_from_filename(file_to_import)
-    LOGGER.info("Will load file '%s' to table '%s'", file_to_import, table)
-    with open(file_to_import, mode='r') as input_file:
-        csvfile = csv.DictReader(input_file)
-        client = DbClient(url, user, password)
-        transformer = get_transformer(table)
-        column_names = transformer.column_names(csvfile.fieldnames)
-        insert_statement = generate_insert(table, column_names)
+class TableLoader:
+    def __init__(self, file_to_import: str, url: str, user: str, password: str):
+        self.file_to_import = file_to_import
+        self.client = DbClient(url, user, password)
+        self.table = self.__table_from_filename(file_to_import)
+        self.counter = 0
+        self.insert_statement = ''
 
-        buffer = []
-        insert_counter = 0
-        for row in csvfile:
-            buffer.append(transformer.column_values(row))
+    def load(self):
+        if self.counter != 0:
+            raise RuntimeError('This instance has already finished inserting')
 
-            if len(buffer) >= BATCH_SIZE:
-                do_insert(client, insert_statement, buffer)
-                insert_counter += len(buffer)
-                LOGGER.debug('Inserted %d into %s', insert_counter, table)
-                buffer = []
+        LOGGER.info("Will load file '%s' to table '%s'", self.file_to_import, self.table)
+        with open(self.file_to_import, mode='r') as input_file:
+            csvfile = csv.DictReader(input_file)
+            transformer = get_transformer(self.table)
+            column_names = transformer.column_names(csvfile.fieldnames)
+            self.insert_statement = self.__generate_insert(self.table, column_names)
 
-        if len(buffer) != 0:
-            do_insert(client, insert_statement, buffer)
-            insert_counter += len(buffer)
-            LOGGER.debug('Inserted %d into %s', insert_counter, table)
+            buffer = []
+            for row in csvfile:
+                buffer.append(transformer.column_values(row))
+                self.__flush_batch_if_required(buffer)
 
-        LOGGER.info("Done")
+            self.__flush_batch_if_required(buffer, force=True)
 
+            LOGGER.info("Done")
 
-def do_insert(client, insert_statement, rows):
-    try:
-        all_res = client.cursor().executemany(insert_statement, rows)
-        for res in all_res:
-            if res['rowcount'] != 1:
-                LOGGER.error("Failed inserting rows: %s", rows)
-                LOGGER.error("Failure was: %s", all_res)
-                raise RuntimeError('Failed inserting batch')
-    except:
-        LOGGER.error("Failed inserting rows: %s", rows)
-        LOGGER.error("Exception was %s",  sys.exc_info()[0])
-        raise
+    def __flush_batch_if_required(self, buffer: list, force=False):
+        if len(buffer) >= BATCH_SIZE or force:
+            self.__do_insert(self.client, self.insert_statement, buffer)
+            self.counter += len(buffer)
+            LOGGER.debug('Inserted %d into %s', self.counter, self.table)
+            buffer.clear()
 
+    @staticmethod
+    def __do_insert(client, insert_statement, rows):
+        try:
+            all_res = client.cursor().executemany(insert_statement, rows)
+            for res in all_res:
+                if res['rowcount'] != 1:
+                    LOGGER.error("Failed inserting rows: %s", rows)
+                    LOGGER.error("Failure was: %s", all_res)
+                    raise RuntimeError('Failed inserting batch')
+        except:
+            LOGGER.error("Failed inserting rows: %s", rows)
+            LOGGER.error("Exception was %s", sys.exc_info()[0])
+            raise
 
-def table_from_filename(filename):
-    table_name = pathlib.Path(filename).name.split('.')[0]
-    return f"{SCHEMA_PREFIX}_{table_name}"
+    @staticmethod
+    def __table_from_filename( filename):
+        table_name = pathlib.Path(filename).name.split('.')[0]
+        return f"{SCHEMA_PREFIX}_{table_name}"
 
-
-def generate_insert(table_name, columns):
-    col_list = ','.join(columns)
-    placeholders = ','.join(['?' for _ in columns])
-    return f"INSERT INTO {table_name} ({col_list}) VALUES ({placeholders})"
+    @staticmethod
+    def __generate_insert( table_name, columns):
+        col_list = ','.join(columns)
+        placeholders = ','.join(['?' for _ in columns])
+        return f"INSERT INTO {table_name} ({col_list}) VALUES ({placeholders})"
 
 
 if __name__ == '__main__':
@@ -77,4 +84,4 @@ if __name__ == '__main__':
     url = sys.argv[2]
     user = sys.argv[3]
     password = sys.argv[4]
-    load(file, url, user, password)
+    TableLoader(file, url, user, password).load()
